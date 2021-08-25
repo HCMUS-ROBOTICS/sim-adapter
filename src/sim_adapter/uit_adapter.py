bf3405a8e4241de330bd0798c01a12e576521956
wan2000 <hoangquan.qti@gmail.com>
2021-08-25 15:34:41 +0700

    add uit simulation adapter

---
+++ src/sim_adapter/uit_adapter.py
@@ -0,0 +1,64 @@
+from .simulator import SimAdapter, Simulator
+from geometry_msgs.msg import Twist
+import numpy as np
+import cv2
+
+import eventlet
+import socketio
+
+from PIL import Image
+from io import BytesIO
+import base64
+
+class UITSimAdapter(SimAdapter):
+    r"""
+    This adapter is aimed to work with UIT-RaceCar's competition simulators.
+
+    These simulators use `socketio` to connect and interact with our platform.
+    """
+
+    def __init__(self, sim: Simulator):
+        self.sim = sim
+
+        self.sio = socketio.Server()
+        self.app = socketio.WSGIApp(self.sio)
+
+        self.speed = 0.0
+        self.angle = 0.0
+
+        self.sio.on('telemetry', self.telemetry)
+        self.sio.on('connect', self.connect)
+
+        eventlet.wsgi.server(eventlet.listen(('', 4567)), self.app)
+
+    def connect(self, sid, environ):
+        print('connect', sid)
+        self.send_control()
+
+    def telemetry(self, sid, data):
+        if data:
+            image = Image.open(BytesIO(base64.b64decode(data["image"])))
+            image = np.asarray(image)
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            self.sim.recv_image(image)
+            self.send_control()
+
+        else:
+            self.sio.emit('manual', data={}, skip_sid=True)
+
+    def send_control(self):
+        self.sio.emit(
+            "steer",
+            data={
+                'steering_angle': str(self.angle),
+                'throttle': str(self.speed),
+            },
+            skip_sid=True)
+
+        self.speed = 0.0
+        self.angle = 0.0
+
+    def send_command(self, cmd_vel: Twist):
+        self.speed = cmd_vel.linear.x
+        self.angle = cmd_vel.angular.z
